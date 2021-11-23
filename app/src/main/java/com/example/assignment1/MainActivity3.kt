b354e46e22b9bcbebec9aad01960122136983eea
nabeelah030 <80572116+nabeelah030@users.noreply.github.com>
2021-11-23 15:47:33 +0500

    assignment2

---
+++ app/src/main/java/com/example/assignment1/MainActivity3.kt
@@ -0,0 +1,20 @@
+package com.example.assignment1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+}
+
+
+
+
+
+
+}
+
